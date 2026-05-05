f785300cd9cbde485362eb32b9bf201afd281cc9
Gabriel Jeronimo <gabrieljvcs@gmail.com>
2026-05-05 11:49:45 -0300

    test: fluxo de agendamentos testado e validado com sucesso

--- config/database.js
+++ config/database.js
@@ -1,5 +1,8 @@
 import { PrismaClient } from '@prisma/client';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
+import 'dotenv/config';
 
-const prisma = new PrismaClient();
+const adapter = new PrismaMariaDb(process.env.DATABASE_URL);
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
